af91f1d2905e324087618c40e1022b3a62acc6cc
Oscar Sims <nxtrafalgar@yahoo.co.nz>
2019-03-27 17:20:15 +1300

    Rebase.

---
+++ karma.conf.js
@@ -0,0 +1,45 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '.',
+        frameworks: ['jasmine', 'karma-typescript'],
+        browsers: ['Electron'],
+        plugins: [
+            'karma-jasmine',
+            'karma-electron',
+            'karma-typescript',
+        ],
+        files: [
+            { pattern: 'spec/**/*.spec.ts' },
+        ],
+        preprocessors: {
+            '**/*.ts': 'karma-typescript'
+        },
+        reporters: ['progress', 'karma-typescript'],
+        singleRun: true,
+        autoWatch: false,
+        karmaTypescriptConfig: {
+            compilerOptions: {
+                target: 'es5',
+                module: 'commonjs',
+                moduleResolution: 'node',
+                isolatedModules: false,
+                jsx: 'react',
+                experimentalDecorators: true,
+                emitDecoratorMetadata: true,
+                declaration: false,
+                noImplicitAny: false,
+                noImplicitUseStrict: false,
+                removeComments: true,
+                noLib: false,
+                preserveConstEnums: true,
+                suppressImplicitAnyIndexErrors: true,
+                strictNullChecks: false,
+                lib: ["ES2015", "DOM"]
+            },
+            bundlerOptions: {
+                entrypoints: /\.spec\.ts$/,
+            },
+            exclude: ['node_modules', 'app']
+        }
+    });
+};
