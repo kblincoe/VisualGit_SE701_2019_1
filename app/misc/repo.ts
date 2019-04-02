2d927e0395532a2e859e240a9a3a191748de4521
Robert Rewcastle <robbierew@gmail.com>
2019-04-02 18:11:06 +1300

    fix initRepo not having a head

--- app/misc/repo.ts
+++ app/misc/repo.ts
@@ -162,23 +162,59 @@ function initRepo(gitignoreTypes: String[]){
     fullLocalPath = require("path").join(__dirname, localPath);
   }  
 
-  Git.Repository.init(fullLocalPath, 0).then(function (repository){
-    console.log("Init called");
-    if (gitignoreTypes.length>0){
-      queryGitignore(gitignoreTypes, function (gitignore) {
-        const fs = require('fs');
-        fs.appendFile(fullLocalPath + "/.gitignore", gitignore, function (err: any) {
-          if (err) {
-            return console.log(err);
-          }  
-          console.log(".gitignore created");
-          openRepository(fullLocalPath,fullLocalPath);
-        }); 
-      })
-    }else{
-      openRepository(fullLocalPath,fullLocalPath);
-    }
+  let createAndAdd = function(){
+    let repository;
+  let index;
+  //Most of this part is based off of 
+  //https://github.com/nodegit/nodegit/blob/master/examples/create-new-repo.js
+  Git.Repository.init(fullLocalPath, 0).then(function(repo) {
+    repository = repo;
+  })
+  .then(function(){
+    return repository.refreshIndex();
+  })
+  .then(function(idx:any) {
+    index = idx;
+  })
+  .then(function() {
+    return index.addByPath(".gitignore");
+  })
+  .then(function() {
+    return index.write();
+  })
+  .then(function() {
+    return index.writeTree();
+  })
+  .then(function(oid:any) {
+    const sign = Git.Signature.default(repository); 
+    // Since we're creating an inital commit, it has no parents. Note that unlike
+    // normal we don't get the head either, because there isn't one yet.
+    return repository.createCommit("HEAD", sign, sign, "add gitignore", oid, []);
+  })
+  .done(function(commitId) {
+    console.log("New Commit: ", commitId);
+    openRepository(fullLocalPath,localPath);
   });
+  }
+
+  const fs = require('fs');
+  //There is a race condition if you try to init the repo while the .gitignore is being written
+  //which is why we have this callback
+  let onWrite = function(err:any){
+    if (err) {
+      return console.log(err);
+    }  
+    console.log(".gitignore created");
+    createAndAdd();
+  }
+  if (gitignoreTypes.length>0){
+    queryGitignore(gitignoreTypes, function (gitignore) {
+      fs.appendFile(fullLocalPath + "/.gitignore", gitignore, onWrite); 
+    })
+  }else{
+    fs.appendFile(fullLocalPath + "/.gitignore", "",onWrite); 
+  }
+ 
 }
 
 function openLocalRepository() {
