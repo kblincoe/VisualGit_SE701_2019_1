5f7c63217d9cac930ce69d030674e49585e80428
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2019-04-03 10:22:25 +1300

    Paste in web example

--- app/components/project.panel.component.ts
+++ app/components/project.panel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DirectoryTreeComponent } from 'ng2-directory-tree'
 
 @Component({
   selector: 'project-panel',
@@ -6,13 +7,35 @@ import { Component } from '@angular/core';
   <div class="project-panel" id="project-panel">
 
     <div class="project-window">
-        hIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+      <directory-tree [directory]="dir" (onChange)="logging($event)" [keyboardWatch]=true></directory-tree>
     </div>
 
   </div>
   `,
+  directives: [DirectoryTreeComponent]
 })
 
 export class ProjectPanelComponent {
 
+  dir = {
+    "name": "photos",
+    "children": [
+      {
+        "name": "summer",
+        "children": [
+          {
+            "name": "june",
+            "children": [
+              {
+                "name": "windsurf.jpg"
+              }]
+          }
+        ]
+      }
+    ]
+  }
+
+  logging(node) {
+      console.log(node)
+  }
 }
