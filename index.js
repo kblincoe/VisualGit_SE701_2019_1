84b32f19760c4f5fdb1f5ff3f8ab3498bbff1b29
lukethompson <lukethompsxn@gmail.com>
2019-04-06 19:50:24 +1300

    move quit option from Mac_Applications to Window

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ const myMenu = [
 		submenu: [
 			{role: 'minimize'},
 			{type: 'separator'},
-			{role: 'close'}
+			// {role: 'close'},
+			{label: "Quit", accelerator: "CmdOrCtrl+Q", click: function () {app.quit();}}
 		]
 	},
 	{
@@ -227,11 +228,6 @@ const myMenu = [
 			{label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:"},
 			{label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
 			{label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"}
-		]
-	}, {
-		label: "Mac_Application",
-		submenu: [
-			{label: "Quit", accelerator: "Command+Q", click: function () {app.quit();}}
 		]
 	}
 ];
