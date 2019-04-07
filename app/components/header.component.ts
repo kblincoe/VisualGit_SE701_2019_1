26e8f023e0c1e55eae43d3c456beca79863eedc8
Nathan Cairns <nathancairns777@gmail.com>
2019-04-07 17:07:47 +1200

    Fixes #220 Make collapsible menu appear on all colour schemes (#252)
    * Make collapsible menu appear on all colour schemes
    
    * Fix linter fails

--- app/components/header.component.ts
+++ app/components/header.component.ts
@@ -8,7 +8,14 @@ import { RepositoryService } from '../services/repository.service';
     <nav class="navbar navbar-inverse" role="navigation">
       <div class="container-fluid row">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+          <button
+            type="button"
+            id="navbarToggle"
+            class="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+          >
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
