c24f508d9922c768bde9af28f3592a83070f5f83
Joel <31730723+joel-clarke@users.noreply.github.com>
2019-04-07 15:01:08 +1200

    Fix #136 jcla776 creating branch with invalid names (#207)
    * Limiting possible branch names
    * There is now restrictions on the name when attempting to create a new branch. This should meet Git branch name standards.
    * Merge fix
    * Various changes
    * Added comments, now works from collapsible menu, branches can now also be created from the collapsible menu.
    * Comment + merge fixes
    * Moved function and deleted useless tooltip
    * Limiting possible branch names
    * Function typo
    * Branch can now end  with "."
    * Fixed merge error

--- app/components/header.component.ts
+++ app/components/header.component.ts
@@ -31,9 +31,9 @@ import { RepositoryService } from '../services/repository.service';
               <ul class="dropdown-menu" id="branch-dropdown" role="menu" aria-labelledby="branch-name">
                 <li role="presentation" id="create-branch">
                   <div class="input-group menuitem">
-                    <input type="text" id="branchName" onkeyup="sortBranches()" class="form-control" placeholder="Search or create branch">
+                    <input type="text" id="branchName" onkeyup="sortBranches(); checkBranch(this)" class="form-control" placeholder="Search or create branch">
                     <span class="input-group-btn">
-                      <button class="btn btn-default" type="button" onclick="createBranch()">OK</button>
+                      <button class="btn btn-default masterTooltip" id = "branch-btn" type="button" disabled="disabled" onclick="createBranch(this)" title="Must meet Git branch standards">OK</button>
                     </span>
                   </div>
                 </li>
@@ -84,9 +84,9 @@ import { RepositoryService } from '../services/repository.service';
               <ul class="dropdown-menu" id="branch-dropdown" role="menu" aria-labelledby="branch-name" style="margin: 5px 20px">
                 <li role="presentation" id="create-branch">
                   <div class="input-group menuitem">
-                    <input type="text" id="branchName" onkeyup="sortBranches()" class="form-control" placeholder="Search or create branch">
+                    <input type="text" id="branchName2" onkeyup="sortBranches(); checkBranch(this)" class="form-control" placeholder="Search or create branch">
                     <span class="input-group-btn">
-                      <button class="btn btn-default" type="button" onclick="createBranch()">OK</button>
+                      <button class="btn btn-default masterTooltip" id = "branch-btn2" type="button" disabled="disabled" title="Must meet Git branch standards" onclick="createBranch(this)">OK</button>
                     </span>
                   </div>
                 </li>
