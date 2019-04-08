4021a902a89117f3d97dbe3196f585539a7c105c
Edward Zhang <edward@zhang.nz>
2019-04-09 00:06:59 +1200

    Fix linter error

--- app/misc/router.ts
+++ app/misc/router.ts
@@ -162,7 +162,7 @@ function disableDiffPanelEditOnHide(){
 }
 
 function clearCloneButtonCache() {
-  var butt = document.getElementById("cloneButton");
+  let butt = document.getElementById('cloneButton');
   butt.innerHTML = 'Clone';
   butt.setAttribute('class', 'btn btn-primary disabled');
 }
