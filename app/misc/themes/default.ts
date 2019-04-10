a574c1242610c35be7499d608993309a6eb26a89
Abhinav Behal <abhinav.behal@hotmail.com>
2019-04-10 22:02:55 +1200

    Fixes #251 Clean up color.ts
    * Refactored the different themes in the application:
    - Moved each theme into its own file
    - Reworked how themes are applied to reduce code duplication
    
    * Changed Theme to ITheme in theme.ts

---
+++ app/misc/themes/default.ts
@@ -0,0 +1,16 @@
+import { ITheme } from './itheme';
+
+export const Default: ITheme = {
+  navbarClass: 'navbar navbar-inverse',
+  iconColor: 'white',
+  filePanelColor: '#282828',
+  paragraphColor: '#CCCCCC',
+  headingColor: '#CCCCCC',
+  diffPanelColor: 'white',
+  diffPanelBackgroundColor: '#282828',
+  networkColor: '#181818',
+  footerColor: '#282828',
+  repositoryPanelColor: '#282828',
+  authenticateColor: '#282828',
+  navbarToggleColor: '#333333',
+};
---
+++ app/misc/themes/pink.ts
@@ -0,0 +1,16 @@
+import { ITheme } from './itheme';
+
+export const Pink: ITheme = {
+  navbarClass: 'navbar navbar-pink',
+  iconColor: 'white',
+  filePanelColor: '#FFC2C2',
+  paragraphColor: '#767676',
+  headingColor: '#FFA3A3',
+  diffPanelColor: 'white',
+  diffPanelBackgroundColor: 'white',
+  networkColor: '#FFE5E5',
+  footerColor: '#FFD7D7',
+  repositoryPanelColor: '#FFD7D7',
+  authenticateColor: '#FFE5E5',
+  navbarToggleColor: '#FFD7D7',
+};
