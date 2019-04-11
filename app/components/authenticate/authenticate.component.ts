901e5b78d40a125e8651dc7c4c578199405de4c5
aorthi <31422522+aorthi@users.noreply.github.com>
2019-04-11 13:08:53 +1200

    Fixes #189 - Alerting user when no username or password given (#291)
    * implementationn of a modal box when user does not input username
    
    * additional logic to handle different input combinations

--- app/components/authenticate/authenticate.component.ts
+++ app/components/authenticate/authenticate.component.ts
@@ -9,6 +9,12 @@ import { Component } from '@angular/core';
 export class AuthenticateComponent {
 
   switchToMainPanel(): void {
+    if (document.getElementById('auth-username').value == ''){
+      displayModal('Please enter your username to sign in');
+    } else if (document.getElementById('auth-password').value == ''){
+      displayModal('Please enter your password to sign in');
+    }
+
     signInPage(switchToAddRepositoryPanel);
   }
 
